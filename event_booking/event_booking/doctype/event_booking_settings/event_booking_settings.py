108551a22f1768ae8bfd103ff07e7a7b7b065b1d
jayaraman <95jayaraman.r63@gmail.com>
2026-03-31 21:39:50 +0530

    Fix issues in event booking settings and related doctypes

--- event_booking/event_booking/doctype/event_booking_settings/event_booking_settings.py
+++ event_booking/event_booking/doctype/event_booking_settings/event_booking_settings.py
@@ -1,10 +1,9 @@
 import frappe
 from frappe.model.document import Document
 
-
 class EventBookingSettings(Document):
 
     def validate(self):
-        if self.tax_percentage is not None:
-            if self.tax_percentage < 0 or self.tax_percentage > 100:
+        if self.default_tax is not None:
+            if self.default_tax < 0 or self.default_tax > 100:
                 frappe.throw("Tax must be between 0 and 100")
