3e1068a520a8b5cb0b273630544a49f6e2156aba
jayaraman <95jayaraman.r63@gmail.com>
2026-04-04 17:51:06 +0530

    Fix issues in Booking Request and Customer Details doctypes

--- event_booking/event_booking/doctype/booking_request/booking_request.js
+++ event_booking/event_booking/doctype/booking_request/booking_request.js
@@ -1,14 +1,10 @@
-let global_tax = 0;
-
 frappe.ui.form.on('Booking Request', {
 
     onload: function(frm) {
 
         frm.set_query("event_category", function () {
             return {
-                filters: {
-                    is_group: 0
-                }
+                filters: { is_group: 0 }
             };
         });
 
@@ -19,15 +15,21 @@ frappe.ui.form.on('Booking Request', {
                 filters: {},
                 fieldname: "default_tax"
             },
-            callback: function(r) {
-                global_tax = r.message.default_tax || 0;
-                calculate_cost(frm);
+            callback: function(res) {
+                frm.tax_rate = (res.message && res.message.default_tax) || 0;
             }
         });
     },
 
-    refresh: function(frm) {
-        calculate_cost(frm);
+    from_date: function(frm) { update_preview(frm); },
+    to_date: function(frm) { update_preview(frm); },
+    venue: function(frm) { update_preview(frm); },
+    profit_margin: function(frm) { update_preview(frm); },
+    discount: function(frm) { update_preview(frm); },
+
+    event_package: function(frm) {
+        load_package_services(frm);
+        update_preview(frm);
     },
 
     before_workflow_action: function(frm) {
@@ -39,26 +41,24 @@ frappe.ui.form.on('Booking Request', {
             frappe.prompt(
                 [
                     {
-                        fieldname: 'rejection_reason',
-                        label: 'Reason for Rejection',
-                        fieldtype: 'Small Text',
+                        fieldname: "rejection_reason",
+                        label: "Reason for Rejection",
+                        fieldtype: "Small Text",
                         reqd: 1
                     }
                 ],
                 function(values) {
 
-                    frm.set_value('rejection_reason', values.rejection_reason);
+                    frm.set_value("rejection_reason", values.rejection_reason);
 
                     frm.save().then(() => {
-
                         frappe.validated = true;
-
                         frm.page.btn_primary.trigger("click");
                     });
 
                 },
-                  ('Enter Rejection Reason'),
-                  ('Submit')
+                "Enter Rejection Reason",
+                "Submit"
             );
         }
     },
@@ -76,89 +76,105 @@ frappe.ui.form.on('Booking Request', {
         frm.set_value("event_package", null);
         frm.clear_table("services");
         frm.refresh_field("services");
-    },
+    }
 
-    event_package: function(frm) {
-        if (frm.doc.event_package) {
-            fetch_package_services(frm);
-            fetch_package_cost(frm);
-        }
-    },
-
-    venue_cost: calculate_cost,
-    package_cost: calculate_cost,
-    profit_margin: calculate_cost,
-    discount: calculate_cost
 });
 
+function load_package_services(frm) {
 
-function fetch_package_services(frm) {
+    if (!frm.doc.event_package) return;
+
     frappe.call({
         method: "event_booking.event_booking.doctype.booking_request.booking_request.get_package_services",
         args: {
             package: frm.doc.event_package
         },
-        callback: function(r) {
-            if (r.message) {
+        callback: function(res) {
 
-                frm.clear_table("services");
+            if (!res.message) return;
 
-                r.message.forEach(item => {
-                    let row = frm.add_child("services");
-                    row.service_provider = item.service_provider;
-                    row.service_type = item.service_type;
-                    row.cost = item.cost;
-                });
+            frm.clear_table("services");
 
-                frm.refresh_field("services");
-            }
+            res.message.forEach(service => {
+                let row = frm.add_child("services");
+                row.service_provider = service.service_provider;
+                row.service_type = service.service_type;
+                row.cost = service.cost;
+            });
+
+            frm.refresh_field("services");
         }
     });
 }
 
+function update_preview(frm) {
 
-function fetch_package_cost(frm) {
+    if (!(frm.doc.venue && frm.doc.from_date && frm.doc.to_date)) return;
+
+    let from = frappe.datetime.str_to_obj(frm.doc.from_date);
+    let to = frappe.datetime.str_to_obj(frm.doc.to_date);
+
+    let total_days = frappe.datetime.get_day_diff(to, from) + 1;
+
     frappe.call({
         method: "frappe.client.get_value",
         args: {
-            doctype: "Event Package",
-            filters: { name: frm.doc.event_package },
-            fieldname: "total_price"
+            doctype: "Venue",
+            filters: { name: frm.doc.venue },
+            fieldname: "daily_rate"
         },
-        callback: function(r) {
-            if (r.message) {
-                frm.set_value("package_cost", r.message.total_price);
-                calculate_cost(frm);
+        callback: function(res) {
+
+            let rate = (res.message && res.message.daily_rate) || 0;
+            let venue_cost = rate * total_days;
+
+            frm.set_value("venue_cost", venue_cost);
+
+            if (frm.doc.event_package) {
+
+                frappe.call({
+                    method: "frappe.client.get_value",
+                    args: {
+                        doctype: "Event Package",
+                        filters: { name: frm.doc.event_package },
+                        fieldname: "total_price"
+                    },
+                    callback: function(pkg) {
+
+                        let pkg_cost = (pkg.message && pkg.message.total_price) || 0;
+
+                        frm.set_value("package_cost", pkg_cost);
+
+                        update_totals(frm);
+                    }
+                });
+
+            } else {
+                update_totals(frm);
             }
         }
     });
 }
 
+function update_totals(frm) {
 
-function calculate_cost(frm) {
+    let venue = flt(frm.doc.venue_cost);
+    let pkg = flt(frm.doc.package_cost);
 
-    let venue_cost = flt(frm.doc.venue_cost);
-    let package_cost = flt(frm.doc.package_cost);
+    let subtotal = venue + pkg;
 
-    let subtotal = venue_cost + package_cost;
+    let margin = flt(frm.doc.profit_margin);
+    let profit = subtotal * (margin / 100);
 
-    let profit_margin = flt(frm.doc.profit_margin);
-    let profit_amount = subtotal * (profit_margin / 100);
+    let tax = subtotal * ((frm.tax_rate || 0) / 100);
 
-    let tax_amount = subtotal * (global_tax / 100);
-
-    let total_amount = subtotal + profit_amount + tax_amount;
+    let total = subtotal + profit + tax;
 
     let discount = flt(frm.doc.discount);
-    let final_amount = total_amount - discount;
+    let final = total - discount;
 
-    frm.set_value("profit_amount", profit_amount);
-    frm.set_value("tax_amount", tax_amount);
-    frm.set_value("total_amount", total_amount);
-    frm.set_value("final_amount", final_amount);
-
-    frm.refresh_field("profit_amount");
-    frm.refresh_field("tax_amount");
-    frm.refresh_field("total_amount");
-    frm.refresh_field("final_amount");
+    frm.set_value("profit_amount", profit);
+    frm.set_value("tax_amount", tax);
+    frm.set_value("total_amount", total);
+    frm.set_value("final_amount", final);
 }
